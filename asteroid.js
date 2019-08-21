43885c0cc1fdab9696d96fc930f2e3bd2817023b
Soap <awesomemw3guy@gmail.com>
2019-08-21 17:27:08 -0500

    added asteroid objects for the player to destroy

--- asteroid.js
+++ asteroid.js
@@ -112,3 +112,45 @@ function render() {
     ship.draw()
     requestAnimationFrame(render);
 }
+
+class Asteroid{
+    constructor(x,y,radius,level,collisionRadius) {
+        this.visible = true;
+        this.x = x || Math.floor(Math.random() * canvasWidth);
+        this.y = y || Math.floor(Math.random() * canvasHeight);
+        this.speed = 3;
+        this.radius = radius || 50;
+        this.angle = Math.floor(Math.random() * 359);
+        this.strokeColor = 'white';
+        this.collisionRadius = collisionRadius || 46;
+        // Used to decide if this asteroid can be broken into smaller pieces
+        this.level = level || 1;  
+    }
+    Update(){
+        let radians = this.angle / Math.PI * 180;
+        this.x += Math.cos(radians) * this.speed;
+        this.y += Math.sin(radians) * this.speed;
+        if (this.x < this.radius) {
+            this.x = canvas.width;
+        }
+        if (this.x > canvas.width) {
+            this.x = this.radius;
+        }
+        if (this.y < this.radius) {
+            this.y = canvas.height;
+        }
+        if (this.y > canvas.height) {
+            this.y = this.radius;
+        }
+    }
+    Draw(){
+        ctx.beginPath();
+        let vertAngle = ((Math.PI * 2) / 6);
+        var radians = this.angle / Math.PI * 180;
+        for(let i = 0; i < 6; i++){
+            ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
