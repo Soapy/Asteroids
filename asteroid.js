ffe7fe8b406ba1568e3d91ecd22f519b5c68b172
Soap <awesomemw3guy@gmail.com>
2019-08-21 17:29:07 -0500

    created bullet objects for the player's ship

--- asteroid.js
+++ asteroid.js
@@ -113,6 +113,29 @@ function render() {
     requestAnimationFrame(render);
 }
 
+class Bullet{
+    constructor(angle) {
+        this.visible = true;
+        this.x = ship.noseX;
+        this.y = ship.noseY;
+        this.angle = angle;
+        this.height = 4;
+        this.width = 4;
+        this.speed = 5;
+        this.velX = 0;
+        this.velY = 0;
+    }
+    Update(){
+        let radians = this.angle / Math.PI * 180;
+        this.x -= Math.cos(radians) * this.speed;
+        this.y -= Math.sin(radians) * this.speed;
+    }
+    Draw(){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+    }
+}
+
 class Asteroid{
     constructor(x,y,radius,level,collisionRadius) {
         this.visible = true;
