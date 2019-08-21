6eaf3a34e16ec8fcc3449c3b37ad1a23f45158f9
Soap <awesomemw3guy@gmail.com>
2019-08-21 17:38:39 -0500

    fixed rendering issues when attempting to draw the player ship

--- asteroid.js
+++ asteroid.js
@@ -10,107 +10,87 @@ document.addEventListener("DOMContentLoaded", setupCanvas);
 function setupCanvas() {
     canvas = document.getElementById("mainCanvas");
     ctx = canvas.getContext("2d");
-
-    canvas.width = this.canvasWidth;
-    canvas.height = this.canvasHeight;
-    
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     ship = new Ship();
+ 
+    for(let i = 0; i < 8; i++){
+        asteroids.push(new Asteroid());
+    }
 
-    //possibly move listeners elsewhere
-    document.body.addEventListener("keydown", function(e){
-        keys[e.keyCode] = true;
-    });
-    document.body.addEventListener("keyup", function(e){
-        keys[e.keyCode] = false;
-    });
-
-    //draws the player's ship and enemies/projectiles
-    render();
+    document.body.addEventListener("keydown", HandleKeyDown);
+    document.body.addEventListener("keyup", HandleKeyUp);
+    Render();
 }
 
 class Ship {
     constructor() {
         this.visible = true;
-
         this.x = canvasWidth / 2;
         this.y = canvasHeight / 2;
-
-        this.isMoving = false;
+        this.movingForward = false;
         this.speed = 0.1;
-        this.velocityX = 0;
-        this.velocityY = 0;
-        this.rotationSpeed = 0.1;
-
-        this.radius = 20;
-        this.anglePos = 0;
-        this.strokeColor = "white";
+        this.velX = 0;
+        this.velY = 0;
+        this.rotateSpeed = 0.001;
+        this.radius = 15;
+        this.angle = 0;
+        this.strokeColor = 'white';
+        this.noseX = canvasWidth / 2 + 15; // bullet fires from the nose of the ship
+        this.noseY = canvasHeight / 2;
     }
 
-    rotate(isClockwise) {
-        let clockwiseValue = isClockwise ? 1 : -1;
-        this.angle += this.rotationSpeed * clockwiseValue;
+    rotate(dir) {
+        this.angle += this.rotateSpeed * dir;
     }
-    
+
     update() {
-        let radians = this.anglePos / Math.PI * 180; //formula for radians
-        if(this.isMoving) {
-            this.velocityX += Math.cos(radians) * this.speed;
-            this.velocityY += Math.sin(radians) * this.speed;
+        let radians = this.angle / Math.PI * 180;
+
+        if (this.movingForward) {
+            this.velX += Math.cos(radians) * this.speed;
+            this.velY += Math.sin(radians) * this.speed;
         }
-        
-        if(this.x < this.radius) {
+        // prevent player's ship from leaving the board
+        if (this.x < this.radius) {
             this.x = canvas.width;
         }
-        if(this.x > canvas.width) {
+        if (this.x > canvas.width) {
             this.x = this.radius;
         }
-        if(this.y < this.radius) {
-            this.x = canvas.height;
+        if (this.y < this.radius) {
+            this.y = canvas.height;
         }
-        if(this.y > canvas.height) {
-            this.x = this.radius;
+        if (this.y > canvas.height) {
+            this.y = this.radius;
         }
-        this.velocityX *= .9;
-        this.velocityY *= .9;
-
-        this.x -= this.velocityX;
-        this.Y -= this.velocityY;
+        // ship slows if no key press is detected
+        this.velX *= 0.99;
+        this.velY *= 0.99;
+   
+        this.x -= this.velX;
+        this.y -= this.velY;
     }
-    
+
     draw() {
         ctx.strokeStyle = this.strokeColor;
         ctx.beginPath();
-
-        let vertexAngle = ((Math.pi * 2) / 3);
-        let radians = this.anglePos / Math.PI * 180;
-
-        for(let i = 0; i < 3; i++) {
-            ctx.lineTo(this.x - this.radius * Math.cos(vertexAngle * i + radians), 
-            this.y - this.radius * Math.sin(vertexAngle * i + radians));
+        // Angle between vertices of the ship
+        let vertAngle = ((Math.PI * 2) / 3);
+ 
+        let radians = this.angle / Math.PI * 180;
+        // Where to fire bullet from
+        this.noseX = this.x - this.radius * Math.cos(radians);
+        this.noseY = this.y - this.radius * Math.sin(radians);
+ 
+        for (let i = 0; i < 3; i++) {
+            ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
         }
         ctx.closePath();
         ctx.stroke();
     }
-}
-
-
-function render() {
-    ship.isMoving = (keys[68]) //keycode for w
-
-    if(keys[68]) { //keycode for a
-        ship.rotate(true);
-    }
-    if(keys[65]) { //keycode for d
-        ship.rotate(false);
-    }
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ship.update();
-    ship.draw()
-    requestAnimationFrame(render);
 }
 
 class Bullet{
@@ -125,12 +105,14 @@ class Bullet{
         this.velX = 0;
         this.velY = 0;
     }
-    Update(){
+
+    update() {
         let radians = this.angle / Math.PI * 180;
         this.x -= Math.cos(radians) * this.speed;
         this.y -= Math.sin(radians) * this.speed;
     }
-    Draw(){
+
+    draw() {
         ctx.fillStyle = 'white';
         ctx.fillRect(this.x,this.y,this.width,this.height);
     }
@@ -149,7 +131,8 @@ class Asteroid{
         // Used to decide if this asteroid can be broken into smaller pieces
         this.level = level || 1;  
     }
-    Update(){
+
+    update() {
         let radians = this.angle / Math.PI * 180;
         this.x += Math.cos(radians) * this.speed;
         this.y += Math.sin(radians) * this.speed;
@@ -166,7 +149,8 @@ class Asteroid{
             this.y = this.radius;
         }
     }
-    Draw(){
+
+    draw(){ 
         ctx.beginPath();
         let vertAngle = ((Math.PI * 2) / 6);
         var radians = this.angle / Math.PI * 180;
@@ -175,5 +159,51 @@ class Asteroid{
         }
         ctx.closePath();
         ctx.stroke();
+    }
+}
+
+function Render() {
+    ship.movingForward = (keys[87]);
+ 
+    if (keys[68]) {
+        // d key rotate right
+        ship.Rotate(1);
+    }
+    if (keys[65]) {
+        // a key rotate left
+       ship.Rotate(-1);
+    }
+   
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+ 
+    if(lives <= 0){
+        document.body.removeEventListener("keydown", HandleKeyDown);
+        document.body.removeEventListener("keyup", HandleKeyUp);
+ 
+        ship.visible = false;
+        ctx.fillStyle = 'white';
+        ctx.font = '50px Arial';
+        ctx.fillText("GAME OVER", canvasWidth / 2 - 150, canvasHeight / 2);
+    }
+
+    if(ship.visible){
+        ship.Update();
+        ship.Draw();
+    }
+    
+    if (bullets.length !== 0) {
+        for(let i = 0; i < bullets.length; i++){
+            bullets[i].Update();
+            bullets[i].Draw();
+        }
+    }
+
+    if (asteroids.length !== 0) {
+        for(let j = 0; j < asteroids.length; j++){
+            asteroids[j].Update();
+            // Pass j so we can track which asteroid points
+            // to store
+            asteroids[j].Draw(j);
+        }
     }
 }
